c8b4edd0018b4d511f3a6884aa15d73d4e03f9ec
倪泽恒 <644508159@qq.com>
2020-11-09 11:46:38 +0800

    添加Helper类。

---
+++ Nzh.Master.Common/Extension/Extension.Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nzh.Master.Common.Extension
+{
+    public static partial class Extensions
+    {
+        public static Exception GetOriginalException(this Exception ex)
+        {
+            if (ex.InnerException == null) return ex;
+
+            return ex.InnerException.GetOriginalException();
+        }
+    }
+}
